ac4bf0061cd6ecaec028d5b33f513847ed937f44
Nidan23 <dev.sulkowski@gmail.com>
2022-09-16 20:35:56 +0200

    [INIT]

---
+++ src/main/kotlin/pl/sulkowski/jakub/atipera/AtiperaApplication.kt
@@ -0,0 +1,11 @@
+package pl.sulkowski.jakub.atipera
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AtiperaApplication
+
+fun main(args: Array<String>) {
+    runApplication<AtiperaApplication>(*args)
+}
