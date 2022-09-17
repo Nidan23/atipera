6eacdf4300dccf82a5d76e7195168efe614e967b
Nidan23 <dev.sulkowski@gmail.com>
2022-09-17 13:23:44 +0200

    [#3][ADD] Added app properties / domain / shared properties values

---
+++ src/main/kotlin/pl/sulkowski/jakub/atipera/domain/Properties.kt
@@ -0,0 +1,13 @@
+package pl.sulkowski.jakub.atipera.domain
+
+// HTTP
+const val HTTP_HEADER_ACCEPT: String = "Accept"
+const val HTTP_HEADER_ACCEPT_VALUE_GITHUB: String = "application/vnd.github+json"
+const val HTTP_HEADER_AUTHORIZATION: String = "Authorization"
+const val HTTP_HEADER_AUTHORIZATION_VALUE_BEARER: String = "Bearer"
+
+// Github
+const val GITHUB_MAIN_URL: String = "https://api.github.com"
+const val GITHUB_USERS_PATH: String = "users"
+const val GITHUB_REPOS_PATH: String = "repos"
+const val GITHUB_BRANCH_URL_SUFFIX: String = "{/branch}"
